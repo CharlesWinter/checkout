79167c1a47c682e9b8f0c9c4ac856fbde2b25493
Charles Winter <charleswinter27@gmail.com>
2020-11-22 12:57:19 +0000

    introduces more failing tests for the deal repository

--- checkout/get_total_price_test.go
+++ checkout/get_total_price_test.go
@@ -81,4 +81,139 @@ func TestGetTotalPrice(t *testing.T) {
 			t.Fatalf("wanted price %d, got %d", dealPrice, got)
 		}
 	})
+
+	t.Run("returns the total price correctly for a basket with a deal and products", func(t *testing.T) {
+		var dealPrice uint = 43
+
+		ctrl := gomock.NewController(t)
+
+		deals := []entities.Deal{
+			{
+				RequiredSubbasket: entities.Basket{
+					itemNameA: 1,
+					itemNameB: 2,
+					itemNameC: 3,
+				},
+				Price: dealPrice,
+			},
+		}
+
+		mockDealGetter := NewMockDealGetter(ctrl)
+		mockDealGetter.EXPECT().GetDeals().Return(deals)
+
+		mockPriceChecker := NewMockPriceChecker(ctrl)
+
+		repo := checkout.New(checkout.RepositoryConfig{
+			DealGetter:   mockDealGetter,
+			PriceChecker: mockPriceChecker,
+		})
+
+		repo.Scan(itemNameA)
+
+		repo.Scan(itemNameB)
+		repo.Scan(itemNameB)
+
+		repo.Scan(itemNameC)
+		repo.Scan(itemNameC)
+		repo.Scan(itemNameC)
+
+		got := repo.GetTotalPrice()
+		if got != dealPrice {
+			t.Fatalf("wanted price %d, got %d", dealPrice, got)
+		}
+	})
+
+	t.Run("if there is an applicable deal and other items in the basket", func(t *testing.T) {
+		var dealPriceA uint = 130
+		var dealPriceB uint = 45
+
+		ctrl := gomock.NewController(t)
+
+		deals := []entities.Deal{
+			{
+				RequiredSubbasket: entities.Basket{
+					itemNameA: 3,
+				},
+				Price: dealPriceA,
+			},
+			{
+				RequiredSubbasket: entities.Basket{
+					itemNameB: 2,
+				},
+				Price: dealPriceB,
+			},
+		}
+
+		mockDealGetter := NewMockDealGetter(ctrl)
+		mockDealGetter.EXPECT().GetDeals().Return(deals)
+
+		mockPriceChecker := NewMockPriceChecker(ctrl)
+		mockPriceChecker.EXPECT().GetItemPrice(itemNameA).Times(1).Return(itemPriceA)
+
+		repo := checkout.New(checkout.RepositoryConfig{
+			DealGetter:   mockDealGetter,
+			PriceChecker: mockPriceChecker,
+		})
+
+		repo.Scan(itemNameB)
+		repo.Scan(itemNameA)
+		repo.Scan(itemNameB)
+
+		got := repo.GetTotalPrice()
+		want := uint(95)
+		if got != want {
+			t.Fatalf("wanted price %d, got %d", want, got)
+		}
+	})
+
+	t.Run("checks that GetTotalPrice is idempotent", func(t *testing.T) {
+		var dealPrice uint = 43
+
+		ctrl := gomock.NewController(t)
+
+		deals := []entities.Deal{
+			{
+				RequiredSubbasket: entities.Basket{
+					itemNameA: 1,
+					itemNameB: 2,
+					itemNameC: 3,
+				},
+				Price: dealPrice,
+			},
+		}
+
+		mockDealGetter := NewMockDealGetter(ctrl)
+		mockDealGetter.EXPECT().GetDeals().Return(deals)
+		mockDealGetter.EXPECT().GetDeals().Return(deals)
+
+		mockPriceChecker := NewMockPriceChecker(ctrl)
+		mockPriceChecker.EXPECT().GetItemPrice(itemNameA).Times(4).Return(itemPriceA)
+		mockPriceChecker.EXPECT().GetItemPrice(itemNameB).Times(2).Return(itemPriceB)
+		mockPriceChecker.EXPECT().GetItemPrice(itemNameC).Times(2).Return(itemPriceC)
+
+		repo := checkout.New(checkout.RepositoryConfig{
+			DealGetter:   mockDealGetter,
+			PriceChecker: mockPriceChecker,
+		})
+
+		repo.Scan(itemNameA)
+		repo.Scan(itemNameA)
+		repo.Scan(itemNameA)
+
+		repo.Scan(itemNameB)
+		repo.Scan(itemNameB)
+		repo.Scan(itemNameB)
+
+		repo.Scan(itemNameC)
+		repo.Scan(itemNameC)
+		repo.Scan(itemNameC)
+		repo.Scan(itemNameC)
+
+		got := repo.GetTotalPrice()
+		got = repo.GetTotalPrice()
+		want := uint(dealPrice + 2*itemPriceA + itemPriceB + itemPriceC)
+		if got != want {
+			t.Fatalf("wanted price %d, got %d", want, got)
+		}
+	})
 }
