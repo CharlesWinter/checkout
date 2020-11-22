611e9c7090824e5e9b2f88c0f06c985b19e65ed6
Charles Winter <charleswinter27@gmail.com>
2020-11-22 10:06:15 +0000

    Introduces tests for getting the total price
    Just a test for the happy path for now, we can add tests for other
    conditions later

--- checkout/get_total_price_test.go
+++ checkout/get_total_price_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/CharlesWinter/checkout/checkout"
+	gomock "github.com/golang/mock/gomock"
 )
 
 const (
@@ -18,13 +19,22 @@ const (
 
 func TestGetTotalPrice(t *testing.T) {
 	t.Run("returns the correct total price for a basket with different items", func(t *testing.T) {
-		c := checkout.New(checkout.RepositoryConfig{})
+		ctrl := gomock.NewController(t)
+
+		mockPriceChecker := NewMockPriceChecker(ctrl)
+		mockPriceChecker.EXPECT().GetItemPrice(itemNameA).Return(itemPriceA)
+		mockPriceChecker.EXPECT().GetItemPrice(itemNameB).Return(itemPriceB)
+		mockPriceChecker.EXPECT().GetItemPrice(itemNameC).Return(itemPriceC)
+
+		c := checkout.New(checkout.RepositoryConfig{
+			PriceChecker: mockPriceChecker,
+		})
 
 		c.Scan(itemNameA)
 		c.Scan(itemNameB)
 		c.Scan(itemNameC)
 
-		var want uint = 100
+		var want uint = itemPriceA + itemPriceB + itemPriceC
 		if got := c.GetTotalPrice(); got != want {
 			t.Fatalf("got %d want %d", got, want)
 		}
