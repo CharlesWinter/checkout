766951d8f0c89351e82c659d415dce43320690b4
Charles Winter <charleswinter27@gmail.com>
2020-11-22 11:53:01 +0000

    Adds code to have the get total price method pass

--- checkout/get_total_price.go
+++ checkout/get_total_price.go
@@ -2,5 +2,9 @@ package checkout
 
 // GetTotalPrice calculates and returns the total price of all items in the basket.
 func (r Repository) GetTotalPrice() uint {
-	return 0
+	var totalPrice uint
+	for name, quantity := range r.basket {
+		totalPrice += quantity * r.priceChecker.GetItemPrice(name)
+	}
+	return totalPrice
 }
